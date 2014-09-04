53e561d25b342f305d0eeff75d9ef1e65db3bf17
Emily P. Cooper <emilypcooper75@gmail.com>
2014-09-03 20:36:13 -0400

    validations and authentications

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,3 +1,5 @@
 class Snack < ActiveRecord::Base
+
+validates :name, :description, presence: true
 belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
+
+  root to: "static_pages#home"
+
   resources :users
 
   resources :snacks
